590540f4b0a20a351ab18b0193546d1b5b3e8a7c
Rubens Felix Malagute <rubens.malagute@omega7systems.com>
2022-06-28 22:47:40 -0300

    adicionando readme

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 from fastapi import FastAPI
 from datetime import date
 import json
@@ -11,4 +10,3 @@ app = FastAPI()
 def get_date():
   toJson = json.dumps({"date": str(date.today())})
   return json.loads(toJson)
-
